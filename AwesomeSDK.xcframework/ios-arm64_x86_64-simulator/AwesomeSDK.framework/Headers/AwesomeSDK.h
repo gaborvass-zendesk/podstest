1d5e2f52857242e9e031b3046594ba63e796a364
Gábor Vass <gabor.vass@zendesk.com>
2021-12-20 12:25:08 +0100

    push binaries

---
+++ AwesomeSDK.xcframework/ios-arm64_x86_64-simulator/AwesomeSDK.framework/Headers/AwesomeSDK.h
@@ -0,0 +1,18 @@
+//
+//  AwesomeSDK_Debug.h
+//  AwesomeSDK-Debug
+//
+//  Created by Jorge Campos on 3/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AwesomeSDK_Debug.
+FOUNDATION_EXPORT double AwesomeSDKVersionNumber;
+
+//! Project version string for AwesomeSDK_Debug.
+FOUNDATION_EXPORT const unsigned char AwesomeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AwesomeSDK_Debug/PublicHeader.h>
+
+
